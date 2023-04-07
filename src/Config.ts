38778a2c95fc1009ad9bca700999b17635a601f4
baochanh18 <44717938+baochanh18@users.noreply.github.com>
2023-04-07 19:20:12 +0900

    docker init

--- src/Config.ts
+++ src/Config.ts
@@ -11,21 +11,21 @@ import {
 class Configuration {
   private readonly logger = new Logger(Configuration.name);
 
-  // @IsString()
-  // readonly AWS_REGION = process.env.AWS_REGION as string || 'AWS_REGION';
+  @IsString()
+  readonly AWS_REGION = process.env.AWS_REGION as string || 'AWS_REGION';
 
-  // @IsOptional()
-  // @IsString()
-  // readonly AWS_ENDPOINT = process.env.AWS_ENDPOINT || 'AWS_ENDPOINT';
+  @IsOptional()
+  @IsString()
+  readonly AWS_ENDPOINT = process.env.AWS_ENDPOINT || 'AWS_ENDPOINT';
 
-  // @IsString()
-  // readonly AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID as string || 'AWS_ACCESS_KEY_ID';
+  @IsString()
+  readonly AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID as string || 'AWS_ACCESS_KEY_ID';
 
-  // @IsString()
-  // readonly AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY as string || 'AWS_SECRET_ACCESS_KEY';
+  @IsString()
+  readonly AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY as string || 'AWS_SECRET_ACCESS_KEY';
 
-  // @IsString()
-  // readonly AWS_SQS_QUEUE_URL = process.env.AWS_SQS_QUEUE_URL as string || 'AWS_SQS_QUEUE_URL';
+  @IsString()
+  readonly AWS_SQS_QUEUE_URL = process.env.AWS_SQS_QUEUE_URL as string || 'AWS_SQS_QUEUE_URL';
 
   @IsBoolean()
   readonly DATABASE_LOGGING = process.env.DATABASE_LOGGING === 'true' || true;
@@ -48,8 +48,8 @@ class Configuration {
   @IsBoolean()
   readonly DATABASE_SYNC = process.env.DATABASE_SYNC === 'true' || true;
 
-  // @IsEmail()
-  // readonly EMAIL = process.env.EMAIL as string || 'test@example.com';
+  @IsEmail()
+  readonly EMAIL = process.env.EMAIL as string || 'test@example.com';
 
   @IsInt()
   readonly PORT = Number(process.env.PORT) || 4000;
