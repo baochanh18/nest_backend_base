47bcafc11642a55e5668d24a2a0e2cf1432bfbcc
baochanh18 <44717938+baochanh18@users.noreply.github.com>
2023-04-07 11:50:45 +0900

    init commit

---
+++ libs/DatabaseModule.ts
@@ -0,0 +1,96 @@
+import { Global, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  DataSource,
+  EntityManager,
+  EntityTarget,
+  ObjectLiteral,
+  QueryRunner,
+  Repository,
+  SelectQueryBuilder,
+} from 'typeorm';
+
+import { Config } from '../src/Config';
+
+import { AccountEntity } from '../src/account/infrastructure/entity/AccountEntity';
+import { NotificationEntity } from '../src/notification/infrastructure/entities/NotificationEntity';
+import { v4 } from 'uuid';
+import { connectionSource } from './DatabaseSource'
+
+interface WriteConnection {
+  readonly startTransaction: (
+    level?:
+      | 'READ UNCOMMITTED'
+      | 'READ COMMITTED'
+      | 'REPEATABLE READ'
+      | 'SERIALIZABLE',
+  ) => Promise<void>;
+  readonly commitTransaction: () => Promise<void>;
+  readonly rollbackTransaction: () => Promise<void>;
+  readonly isTransactionActive: boolean;
+  readonly manager: EntityManager;
+}
+
+interface ReadConnection {
+  readonly getRepository: <T extends ObjectLiteral>(
+    target: EntityTarget<T>,
+  ) => Repository<T>;
+  readonly query: (query: string) => Promise<void>;
+  readonly createQueryBuilder: <Entity extends ObjectLiteral>(
+    entityClass: EntityTarget<Entity>,
+    alias: string,
+    queryRunner?: QueryRunner,
+  ) => SelectQueryBuilder<Entity>;
+}
+
+export let writeConnection = {} as WriteConnection;
+export let readConnection = {} as ReadConnection;
+
+class DatabaseService implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit(): Promise<void> {
+    await connectionSource.initialize();
+    if (connectionSource.isInitialized)
+      throw new Error('DataSource is not initialized');
+    writeConnection = connectionSource.createQueryRunner();
+    readConnection = connectionSource.manager;
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await connectionSource.destroy();
+  }
+}
+
+export class EntityId extends String {
+  constructor() {
+    super(v4().split('-').join(''));
+  }
+}
+
+export const ENTITY_ID_TRANSFORMER = 'EntityIdTransformer';
+
+export interface EntityIdTransformer {
+  from: (dbData: Buffer) => string;
+  to: (stringId: string) => Buffer;
+}
+
+class EntityIdTransformerImplement implements EntityIdTransformer {
+  from(dbData: Buffer): string {
+    return Buffer.from(dbData.toString('binary'), 'ascii').toString('hex');
+  }
+
+  to(entityData: string): Buffer {
+    return Buffer.from(entityData, 'hex');
+  }
+}
+
+@Global()
+@Module({
+  providers: [
+    DatabaseService,
+    {
+      provide: ENTITY_ID_TRANSFORMER,
+      useClass: EntityIdTransformerImplement,
+    },
+  ],
+  exports: [ENTITY_ID_TRANSFORMER],
+})
+export class DatabaseModule {}
---
+++ libs/DatabaseSource.ts
@@ -0,0 +1,22 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { DataSource } from 'typeorm';
+import { AccountEntity } from '../src/account/infrastructure/entity/AccountEntity';
+import { NotificationEntity } from '../src/notification/infrastructure/entities/NotificationEntity';
+import { Config } from '../src/Config';
+
+export const connectionSource = new DataSource({
+  name: 'default',
+  type: 'mysql',
+  charset: 'utf8mb4_unicode_ci',
+  logging: Config.DATABASE_LOGGING,
+  host: Config.DATABASE_HOST,
+  port: Config.DATABASE_PORT,
+  database: Config.DATABASE_NAME,
+  username: Config.DATABASE_USER,
+  password: Config.DATABASE_PASSWORD,
+  synchronize: Config.DATABASE_SYNC,
+  cache: true,
+  entities: [AccountEntity, NotificationEntity],
+  migrations: ['../migrations/*.ts'],
+  namingStrategy: new SnakeNamingStrategy(),
+});
